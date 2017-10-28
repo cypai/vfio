5c2633ae409d7284200aad4eaf1827cae51c0bd1
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-10-28 12:21:26 -0700

    ehh sort of working

--- qemu.sh
+++ qemu.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-sudo qemu-system-x86_64 -enable-kvm -m 6G -boot /dev/mapper/lmhdd-windows,format=raw
+# use pulseaudio
+export QEMU_AUDIO_DRV=pa
+export QEMU_PA_SAMPLES=8192
+export QEMU_AUDIO_TIMER_PERIOD=99
+export QEMU_PA_SERVER=/run/user/1000/pulse/native
+
+sudo qemu-system-x86_64 \
+    -enable-kvm \
+    -m 6G \
+    -cpu host \
+    -smp 4,sockets=1,cores=2,threads=2 \
+    -mem-prealloc \
+    -balloon none \
+    -rtc clock=host,base=localtime \
+    -serial none \
+    -parallel none \
+    -soundhw hda \
+    -boot dc \
+    /dev/mapper/lmhdd-windows
