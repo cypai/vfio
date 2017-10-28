3912e804ee3b0eabf27393efa09c09cb055874a5
Cheng-Yu Pai <mail4charliepai@gmail.com>
2017-10-28 12:26:58 -0700

    reinstall?

--- qemu.sh
+++ qemu.sh
@@ -7,10 +7,12 @@ export QEMU_AUDIO_TIMER_PERIOD=99
 export QEMU_PA_SERVER=/run/user/1000/pulse/native
 
 sudo qemu-system-x86_64 \
+    -name "windows7vm",process="windows7vm" \
+    -machine type=q35,accel=kvm \
+    -cpu host,kvm=off \
+    -smp 6,sockets=1,cores=3,threads=2 \
     -enable-kvm \
     -m 6G \
-    -cpu host \
-    -smp 4,sockets=1,cores=2,threads=2 \
     -mem-prealloc \
     -balloon none \
     -rtc clock=host,base=localtime \
@@ -18,4 +20,6 @@ sudo qemu-system-x86_64 \
     -parallel none \
     -soundhw hda \
     -boot dc \
+    -device virtio-scsi-pci,id=scsi \
+    -drive file=./Win7_Ult_SP1_English_x64.iso,id=isocd,format=raw,if=none -device scsi-cd,drive=isocd \
     /dev/mapper/lmhdd-windows
